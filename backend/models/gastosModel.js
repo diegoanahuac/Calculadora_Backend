4ac33f35461833d8e6fdcfb91bc29efddb3d598e
Diego Olea <diegoolea@Diegos-MacBook-Pro.local>
2026-03-10 21:07:53 -0600

    Cambios realizados

---
+++ backend/models/gastosModel.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose')
+
+const gastosSchema = mongoose.Schema({
+    description: {
+        type: String,
+        required: [true, 'Teclea una descripción']
+    },
+    importe: {
+        type: Number,
+        required: [true, 'Teclea el importe']
+    }
+},{
+    timestamps: true 
+})
+
+module.exports = mongoose.model('Gasto', gastosSchema)
--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,29 @@
 const express = require('express')
 const color = require('colors')
-const dotenv = require('dotenv').config ()
+const dotenv = require('dotenv').config()
 const connectDB = require('./config/db')
 const port = process.env.PORT || 5001
+const {errorHandler} = require('./middleware/errorMiddleware')
+const cors = require('cors')
 
 connectDB()
 
 const app = express()
+
+/*const corsOptions = {
+    origin: 'https//midominio.com',
+    optionsSuccessStatus: 200
+}
+
+app.use(cors(corsOptions))
+*/
+
+app.use(cors())
+
+app.use(express.json())
+
+app.use('/api/gastos', require('./routes/gastosRoutes'))
+
+app.use(errorHandler)
+
 app.listen(port, () => console.log(`Server running on port ${port}`))
